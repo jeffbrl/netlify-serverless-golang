ab6228d909dd5d346e1eaa1319b724932ec4414d
Jeff Loughridge <jeffl@konekti.us>
2021-02-07 17:20:11 -0500

    Examine query string

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
+	name := request.QueryStringParameters["name"]
+	response := fmt.Sprintf("Hello %s!", name)
+
+	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Headers:    map[string]string{"Content-Type": "text/html; charset=UTF-8"},
+		Body:       response,
 	}, nil
 }
 
